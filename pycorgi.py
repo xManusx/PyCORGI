230a6c906eeca7abfe44f5162d3e0c4570fbed58
Dennis Scheffer <deepsheaper@googlemail.com>
2017-01-19 18:06:11 +0100

    added librosa for chroma feature extraction and visualization

--- pycorgi.py
+++ pycorgi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.6
+#!/usr/bin/env python
 
 import scipy.io.wavfile as wav
 import argparse
@@ -6,23 +6,32 @@ import transformer
 import matplotlib.pyplot as plt
 import numpy as np
 import templates
+import librosa
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="A WAV File, desireably containing some music")
 parser.add_argument("-N", "--windowlength", help="Windowlength to use for the STFT [seconds]", type=float, default=0.2)
 args = parser.parse_args()
 
-samplerate,samples = wav.read(args.file)
+samples, samplerate = librosa.core.load(args.file)
 windowsize = args.windowlength*samplerate
 hopsize = 0.5*windowsize
 
 #trans = transformer.Transformer(4400, 2200, samplerate)
 #stft = trans.stft(samples)
 #gpec = trans.spectrogram(stft)
+
 #trans.plotSpectrogram(spec)
 #log = trans.logFrequencySpectrogram(spec)
 #plt.imshow(np.transpose(log))
 #plt.show()
 #chromas = trans.chroma(log)
-#plt.imshow(np.transpose(chromas))
+chromas = librosa.feature.chroma_stft(y=samples, sr=samplerate, n_fft=windowsize, hop_length=hopsize)
+print(chromas.shape)
+#plt.subplot(4, 2, 5)
+librosa.display.specshow(chromas, y_axis='chroma')
+plt.colorbar()
+plt.title('Chromagram')
+plt.show()
+#plt.imshow(chromas)
 #plt.show()
